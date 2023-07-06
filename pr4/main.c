f6eb06806635b85bc86cc04ea267472668c0fcc8
mxxglad <120576248+mxxglad@users.noreply.github.com>
2023-07-06 20:50:49 +0300

    Add files via upload

---
+++ pr4/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+
+    printf("Enter the n number: ");
+    scanf("%u", &n);
+
+    for (int i = 1; i <= n; i++) {
+        if (n%i == n/i) {
+            printf("%d",i);
+            count++;
+        }
+    }
+
+    printf("The number of equal divisors of number n: %u\n", count);
+
+    return 0;
+}
